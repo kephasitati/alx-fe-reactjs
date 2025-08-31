b4edbe7ab84e5231da982fff1c1ab1b3ac844641
Kepha Sitati <kephasitati@gmail.com>
2025-08-31 23:45:49 +0300

    Add UserProfile component with props

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,11 +1,20 @@
+import PropTypes from 'prop-types';
+import './UserProfile.css';
+
 const UserProfile = ({ name, age, bio }) => {
     return (
-        <div>
+        <div className="user-profile">
             <h2>{name}</h2>
             <p>Age: {age}</p>
             <p>Bio: {bio}</p>
         </div>
     );
+};
+
+UserProfile.propTypes = {
+    name: PropTypes.string.isRequired,
+    age: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+    bio: PropTypes.string.isRequired,
 };
 
 export default UserProfile;
