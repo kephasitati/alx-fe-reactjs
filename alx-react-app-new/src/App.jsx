11355275edec968c248d00e168ee94691b3ee6e7
Kepha Sitati <kephasitati@gmail.com>
2025-09-16 23:06:49 +0300

    recipe sharing app

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,36 +1,43 @@
 import Header from './components/Header';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
-import Counter from './components/Counter';
+import ProfilePage from './components/ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
+  const userData = { name: 'Jane Doe', email: 'jane.doe@example.com' };
+
   return (
-    <div
-      style={{
-        fontFamily: 'Arial, sans-serif',
-        minHeight: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-      }}
-    >
-      <Header />
+    <UserContext.Provider value={userData}>
       <div
         style={{
-          flex: '1',
-          padding: '10px',
+          fontFamily: 'Arial, sans-serif',
+          minHeight: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
         }}
       >
-        <UserProfile
-          name="John Doe"
-          age={30}
-          bio="Loves traveling and photography."
-        />
-        <Counter />
-        <MainContent />
+        <Header />
+        <div
+          style={{
+            flex: '1',
+            padding: '10px',
+          }}
+        >
+          <UserProfile
+            name="John Doe"
+            age={30}
+            bio="Loves traveling and photography."
+          />
+          <Counter />
+          <ProfilePage />
+          <MainContent />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </UserContext.Provider>
   );
 }
 
